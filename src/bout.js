dda1ee7af4899c1f9fe58984fd943e20f9e0d62e
Dale Wick <dmwick@yahoo.com>
2022-05-13 23:52:43 -0400

    Scale checking of character sheet and terrain
    - Draws placeholder image.

--- src/bout.js
+++ src/bout.js
@@ -16,6 +16,8 @@ export default class Bout extends Phaser.Scene {
         this.load.json('graveyard', 'assets/map/graveyard.json');
         this.load.spritesheet('tiles', 'assets/map/iso-64x64-outside.png', {frameWidth: 64, frameHeight: 64});
 
+        this.stewie = this.load.spritesheet('stewie', 'assets/character/people-preview.png', { frameWidth: 64, frameHeight: 96 });
+
         /*this.load.image('sky', 'assets/sprites/sky.png');
 
         this.load.image('syllable-do','assets/hud/syllable-do.png');
@@ -55,71 +57,34 @@ export default class Bout extends Phaser.Scene {
         // Animation set
         this.anims.create({
             key: name+'walk',
-            frames: this.anims.generateFrameNumbers(name, { frames: [ 0, 1, 2, 3 ] }),
+            frames: this.anims.generateFrameNumbers(name, { frames: [ 1, 2, 3, 4, 5, 6, 7, 8 ] }),
             frameRate: 8,
             repeat: -1
         });
 
         this.anims.create({
             key: name+'idle',
-            frames: this.anims.generateFrameNumbers(name, { frames: [ 5, 6, 7, 8 ] }),
-            frameRate: 8,
+            frames: this.anims.generateFrameNumbers(name, { frames: [ 0,0,0,0,0,0,0,0,4 ] }),
+            frameRate: 4,
             repeat: -1
         });
 
         this.anims.create({
-            key: name+'kick',
-            frames: this.anims.generateFrameNumbers(name, { frames: [ 10, 11, 12, 13, 10 ] }),
+            key: name+'attack',
+            frames: this.anims.generateFrameNumbers(name, { frames: [ 0, 9, 9, 0 ] }),
             frameRate: 8,
             repeat: -1,
             repeatDelay: 2000
         });
 
-        this.anims.create({
-            key: name+'punch',
-            frames: this.anims.generateFrameNumbers(name, { frames: [ 15, 16, 17, 18, 17, 15 ] }),
-            frameRate: 8,
-            repeat: -1,
-            repeatDelay: 2000
-        });
-
-        this.anims.create({
-            key: name+'shield',
-            frames: this.anims.generateFrameNumbers(name, { frames: [ 15, 16, 17, 18, 18, 18, 18, 18, 17, 15 ] }),
-            frameRate: 8,
-            repeat: -1,
-            repeatDelay: 2000
-        });
-
-        this.anims.create({
-            key: name+'jump',
-            frames: this.anims.generateFrameNumbers(name, { frames: [ 20, 21, 22, 23 ] }),
-            frameRate: 8,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: name+'jumpkick',
-            frames: this.anims.generateFrameNumbers(name, { frames: [ 20, 21, 22, 23, 25, 23, 22, 21 ] }),
-            frameRate: 8,
-            repeat: -1
-        });
-
-        this.anims.create({
-            key: name+'win',
-            frames: this.anims.generateFrameNumbers(name, { frames: [ 30, 31 ] }),
-            frameRate: 8,
-            repeat: -1,
-            repeatDelay: 2000
-        });
 
         this.anims.create({
             key: name+'die',
-            frames: this.anims.generateFrameNumbers(name, { frames: [ 35, 36, 37 ] }),
+            frames: this.anims.generateFrameNumbers(name, { frames: [ 0 ] }),
             frameRate: 8,
         });
 
-        const keys = [ 'walk', 'idle', 'kick', 'punch', 'jump', 'jumpkick', 'win', 'die' ];
+        const keys = [ 'walk', 'idle', 'attack', 'die' ];
     }
 
     create ()
@@ -128,11 +93,13 @@ export default class Bout extends Phaser.Scene {
         this.buildMap();
 
         this.cameras.main.setSize(4000, 600);
-        /*this.add.image(0, 0, 'sky').setOrigin(0, 0);
+        //this.add.image(0, 0, 'sky').setOrigin(0, 0);
 
         this.createAnim('stewie');
-        this.playerSprite = this.add.sprite(200,400);
-        this.playerSprite.setScale(4);
+        this.playerSprite = this.add.sprite(360+116,360+76);
+        this.playerSprite.depth = 10000;
+		console.log("The player sprite depth is " + this.playerSprite.depth);
+		//this.playerSprite.setScale(4);
         this.playerSprite.play('stewieidle');
         this.playerSprite.flipX = true;
 
@@ -141,6 +108,7 @@ export default class Bout extends Phaser.Scene {
         var health=30;
 
         this.player = new Player({scene:this, sprite: this.playerSprite, x: x, y: y, health: health});
+		/*
         x=600;
         this.npc = new Npc({scene: this, sprite: this.npcSprite, x:x, y:y, health: health});
         this.hud = new Hud({scene: this, player: this.player, npc: this.npc});
@@ -154,7 +122,7 @@ export default class Bout extends Phaser.Scene {
 
         /*this.createSounds();*/
 
-        //this.player.create();
+        this.player.create();
         //this.hud.create();
         //this.npc.create();
     }
@@ -185,11 +153,9 @@ export default class Bout extends Phaser.Scene {
               const tx = (x-y) * tileWidthHalf;
               const ty = (x+y) * tileHeightHalf;
               if(id != -1){
-              const tile = scene.add.image(centerX + tx, centerY + ty, 'tiles', id);
+				  const tile = scene.add.image(centerX + tx, centerY + ty, 'tiles', id);
 
-              tile.depth = centerY + ty;
-
-
+				  tile.depth = centerY + ty;
               }
               i++;
           }
@@ -263,7 +229,7 @@ export default class Bout extends Phaser.Scene {
           d = 1;
         }
       }*/
-      //this.player.update();
+      this.player.update();
       // Use actor for the animated figures.  Each player or npc has an actor.  This updates the player + npc.
       //this.hud.update();
     }
